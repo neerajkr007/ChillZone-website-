40e0b2bc254ae7c86a3523fe7c628a2443f40b70
neerajkr007 <neerajkr007@users.noreply.github.com>
2020-11-10 17:58:21 +0530

    all pages load in backend

--- app.js
+++ app.js
@@ -9,6 +9,36 @@ app.get('/', (req, res) =>
     res.sendFile(__dirname + '/index.html');
 }); 
 
+app.get('/index.html', (req, res) =>
+{
+    res.sendFile(__dirname + '/index.html');
+}); 
+
+app.get('/game1.html', (req, res) =>
+{
+    res.sendFile(__dirname + '/game1.html');
+}); 
+
+app.get('/game2.html', (req, res) =>
+{
+    res.sendFile(__dirname + '/game2.html');
+}); 
+
+app.get('/game3.html', (req, res) =>
+{
+    res.sendFile(__dirname + '/game3.html');
+}); 
+
+//  app.get('', (req, res) =>
+//  {
+//      res.sendFile(__dirname + '/notice.html');
+//  });
+
+app.get('/notice.html', (req, res) =>
+{
+    res.sendFile(__dirname + '/notice.html');
+}); 
+
 app.use(express.static(__dirname + '/public'));
 
 serv.listen(process.env.PORT || 3000);
