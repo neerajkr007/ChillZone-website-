5e6efda333a4731660defdcecd849da127bd2b81
neerajkr007 <neerajkr007@users.noreply.github.com>
2020-11-11 10:04:25 +0530

    testing socket.io

--- app.js
+++ app.js
@@ -44,45 +44,9 @@ app.use(express.static(__dirname + '/public'));
 serv.listen(process.env.PORT || 3000);
 
 
-/*console.log("start");
-const http = require('http');
-const fs = require('fs');
-const port = 3000;
-app.use(express.static('data'));
-const home = fs.readFileSync('./index.html');
-const about = fs.readFileSync('./notice.html');
-const game1 = fs.readFileSync('./game1.html');
-const game2 = fs.readFileSync('./game2.html');
-const game3 = fs.readFileSync('./game3.html');
 
-const server = http.createServer((req, res)=>{
-    console.log(req.url);
-    url = req.url;
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    if(url == '/'){
-        res.end(home);
-    }
-    else if(url == '/game1.html'){
-        res.end(game1);
-    }
-    else if(url == '/game2.html'){
-        res.end(game2);
-    }
-    else if(url == '/game3.html'){
-        res.end(game3);
-    }
 
-    else{
-        res.statusCode = 404;
-        res.end("<h1>404 not found</h1>");
-    }
-});
-
-server.listen(port, () => {
-    //console.log(`Server running at http://${hostname}:${port}/`);
-  });
-
-console.log("end");
-*/
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket){
+	console.log('socket connection');});  
